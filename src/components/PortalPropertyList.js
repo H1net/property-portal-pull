27faa8d36ab4ab84f42638ff4995feb6b6c54534
Ben <ben@freshdevs.com>
2018-04-19 14:08:12 +0100

    Work on api

--- src/components/PortalPropertyList.js
+++ src/components/PortalPropertyList.js
@@ -12,20 +12,30 @@ class PortalPropertyList extends Component {
     let propertyId = 64326220;
     let apiURL = 'http://api.rightmove.co.uk/api/propertyDetails?propertyId=' + propertyId + '&apiApplication=IPAD';
     console.log("apiURL", apiURL);
-    fetch(apiURL, {mode: 'no-cors'})
-    .then(results => results.json())
-    .then(data => {
-      // console.log(data);
-      // let properties = data.results.map((property) => {
-      //   return(
-      //     <div key="{property.results}">
-      //       {/* {property.name} */}
-      //     </div>
-      //   )
-      // })
-      // this.setState(properties: properties);
-      // console.log("state", this.state.properties);
-    })
+    
+    // fetch(apiURL, {mode: 'no-cors'})
+    // .then(results => results.json())
+    // .then(data => {
+    //   // console.log(data);
+    //   // let properties = data.results.map((property) => {
+    //   //   return(
+    //   //     <div key="{property.results}">
+    //   //       {/* {property.name} */}
+    //   //     </div>
+    //   //   )
+    //   // })
+    //   // this.setState(properties: properties);
+    //   // console.log("state", this.state.properties);
+    // })
+
+    fetch(apiURL, {credentials: 'include'}).then(function(response) {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error('Network response was not ok.');
+    }).then(results => results.json()).catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ', error.message);
+    });
   }
 
   render () {
